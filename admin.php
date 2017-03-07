9be1f108554e9bcf3a58f61c9f14af0720e4488b
EvilTwin01 <mohdariffin.work@gmail.com>
2017-03-08 04:21:29 +0800

    admin login

---
+++ admin.php
@@ -0,0 +1,41 @@
+<?php 
+	session_start();
+	if(session_id()=='' || isset($_SESSION['username'])){
+?>
+
+<!doctype html>
+<html>
+<head>
+<link href="admin.css" rel="stylesheet" type="text/css">
+<meta charset="utf-8">
+<title>Admin | Dashboard</title>
+</head>
+
+<body style="background-color: #F9F9F9">
+
+<h2 class="h2">Coffee Corner</h2>
+<nav class="navbar">
+  <ul class="ul">
+	  <li class="dashboard"><a href="admin.php">Dashboard</a></li>
+	  <li class="add"><a>Add notification</a></li>
+	  <li class="view"><a>View Reservation</a></li>
+	  <li class="update"><a>Reservation Status</a></li>
+	  <li class="delete"><a>Delete Reservation</a></li>
+	  <li class="border-bottom"><a></a></li>
+  </ul>
+</nav>
+<div>
+	<h3 class="h3">Welcome <?php //echo $_SESSION['username']. "."; ?></h3>
+</div>
+	<p class="credential">Logged in as : <?php //echo $_SESSION['username']; ?></p>
+	<a class="button_logout" href="logout.php" name="logout">Log out</a>
+</body>
+
+<?php 
+ } 
+ else
+ { 
+	header("location: login.php");
+ }
+?>
+</html>
---
+++ admin_login.php
@@ -0,0 +1,74 @@
+<?php
+  session_start();
+  $dbhost = "localhost";
+  $dbuser = "root";
+  $dbpass = "1234";
+  $dbname = "coffeecorner";
+  $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+  if(isset($_POST['login']))
+  {
+	  $username = mysqli_real_escape_string($connection, $_POST['username']);
+	  $password = mysqli_real_escape_string($connection, $_POST['password']);
+	  
+	  $sql = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+	  $result = mysqli_query($connection, $sql);
+	  
+	  if(!$result)
+			{
+				die("database query fail!" . mysqli_error($connection));
+			}
+
+	  if(mysqli_num_rows($result) == 1)
+	  {
+		  header("location: admin.php");
+	  }
+	  else
+	  {
+		echo "Username/password are incorrect!";
+	  }
+  }
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Admin | Login</title>
+<link href="admin_login.css" rel="stylesheet" type="text/css">
+</head>
+
+<body class="background">
+	<h1 class="h2">Coffee Corner</h1>
+	<h3 class="h3">ADMIN LOGIN</h3>
+<div class="box">
+	<form method="post" action="admin_login.php" >
+		<?php 
+		if(isset($_POST['login']))
+  		{
+	  		$username = mysqli_real_escape_string($connection, $_POST['username']);
+			$password = mysqli_real_escape_string($connection, $_POST['password']);
+	  
+			$sql = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+	  		$result = mysqli_query($connection, $sql);
+	  
+	  		if(mysqli_num_rows($result) == 1)
+	  		{
+				echo "";
+				$_SESSION['username'] = $username;
+	  		}
+	  		else
+	  		{
+				echo "Incorrect username/password!";
+	  		}
+  		}
+		?>
+		<p class="username">USERNAME</p>
+		<input type="text" class="username" name="username" required><br><br>
+		<p class="password">PASSWORD</p>
+		<input type="password" class="password" name="password" required><br><br>
+		<input class="sign" type="submit" class="login_button" value="SIGN IN" name="login">
+	 </form>
+</div>
+</body>
+</html>
